4cd58b8149b1a37e00a4af298567171e408b5de5
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-07-22 18:15:38 +0530

    [Saravanan]: Added program to find length of the line

--- src/LineComparisonProgram.java
+++ src/LineComparisonProgram.java
@@ -3,6 +3,16 @@ public class LineComparisonProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation Program");
+		// variables
+		int x1 = 5;
+	    int x2 = 6;
+	    int y1 = 7;
+	    int y2 = 8;
+	    
+	    //Logic to find length of line
+	    double lengthOfLine = Math.sqrt((Math.pow((x2-x1), 2) + (Math.pow((y2-y1), 2))));
+        System.out.println("Length of the line: " +lengthOfLine);
 
 	}
 
