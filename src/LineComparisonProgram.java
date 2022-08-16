b04405faa05ff03c6cc29d53faaf533fdf5dc0d7
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-16 19:09:40 +0530

    [Saravanan]: Added program for line comparison using oops

--- src/LineComparisonProgram.java
+++ src/LineComparisonProgram.java
@@ -1,40 +1,114 @@
-
+/*
+ * This program using Java Object Oriented Programming Concepts of Line and Point as well as equals and compareTo methods.
+ */
 public class LineComparisonProgram {
+    public static void main(String[] args) {
+        Line l1 = new Line(0, 0, 0, 10);
+        Line l2 = new Line(10, 10, 10, 10);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Line Comparison Computation Program");
-		// variables
-		int x1 = 5;
-	    int x2 = 6;
-	    int y1 = 7;
-	    int y2 = 8;
-	    int a1 = 6;
-	    int a2 = 8;
-	    int b1 = 8;
-	    int b2 = 10;
-	    
-	    // Logic to find length of line
-	    double lengthOfFirstLine = Math.sqrt((Math.pow((x2-x1), 2) + (Math.pow((y2-y1), 2))));
-	    System.out.println("Length of the first line: " +lengthOfFirstLine);
-	        
-	    double lengthOfSecondLine = Math.sqrt((Math.pow((a2-a1), 2) + (Math.pow((b2-b1), 2))));
-	    System.out.println("Length of the second line: " +lengthOfSecondLine);
-	    
-	    // Creating objects for length of line
-	    Integer obj1 = new Integer ((int) lengthOfFirstLine);
-	    Integer obj2 = new Integer ((int) lengthOfSecondLine);
-	    
-	    // Compare both lines with compareTo method
-	    if (obj1.compareTo(obj2) > 0) {
-        	System.out.println("The length of the first line is greater");
+        System.out.println(l1.equals(l2));
+        System.out.println(lineDescription(l1.compareTo(l2)));
+    }
+    
+    /*
+     * Logic to see greater line
+     */
+    public static String lineDescription(int val) {
+        if (val > 0) {
+            return "Line 1 Greater";
+        } else if (val < 0) {
+            return "Line 1 Smaller";
+        } else {
+            return "Line 1 and Line 2 are equal";
         }
-	    else if (obj1.compareTo(obj2) < 0) {
-	    	System.out.println("The length of the second line is greater");
-	    }
-        else {
-        	System.out.println("The both lines are equal");
-        }
-	}
+    }
+       
 
 }
+
+class Line {
+	private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    
+    public Line() {
+        super();
+    }
+    
+    public Line(int x1, int y1, int x2, int y2) {
+        super();
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+    
+    public boolean equals(Object obj) {
+        Line other = (Line) obj;
+        // line 1
+        double line1x = Math.pow((x2 - x1), 2);
+        double line1y = Math.pow((y2 - y1), 2);
+        double length1 = Math.sqrt(line1x + line1y);
+        // line 2
+        double line2x = Math.pow((other.x2 - other.x1), 2);
+        double line2y = Math.pow((other.y2 - other.y1), 2);
+        double length2 = Math.sqrt(line2x + line2y);
+
+        if (length1 == length2) {
+        	System.out.println("Both lines are equal");
+            return true;
+        }else{
+        	System.out.println("Both lines are equal");
+        	return false;
+        }
+    
+	}
+    public int compareTo(Object o) {
+        Line other = (Line) o;
+        // line 1
+        double line1x = Math.pow((x2 - x1), 2);
+        double line1y = Math.pow((y2 - y1), 2);
+        double length1 = Math.sqrt(line1x + line1y);
+        // line 2
+        double line2x = Math.pow((other.x2 - other.x1), 2);
+        double line2y = Math.pow((other.y2 - other.y1), 2);
+        double length2 = Math.sqrt(line2x + line2y);
+
+        return (int) (length1 - length2);
+    }
+		
+}
+
